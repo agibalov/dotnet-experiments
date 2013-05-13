7c3a4ea4ee14e86194d375df101f824e6d4da954
unknown <andrey.agibalov@gmail.com>
2013-05-13 12:24:05 +0400

    Initial version

---
+++ EFCodeFirstMigrationsCIExperiment/BasicTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace EFCodeFirstMigrationsCIExperiment
+{
+    [TestFixture]
+    public class BasicTests
+    {
+        [Test]
+        public void CanCreateNewUser()
+        {
+            int originalUserCount;
+            using (var context = new MyContext())
+            {
+                originalUserCount = context.Users.Count();
+            }
+
+            using (var context = new MyContext())
+            {
+                var user = new User
+                    {
+                        UserName = string.Format("User #{0}", Guid.NewGuid().ToString())
+                    };
+                context.Users.Add(user);
+                context.SaveChanges();
+            }
+
+            int newUserCount;
+            using (var context = new MyContext())
+            {
+                newUserCount = context.Users.Count();
+            }
+
+            Assert.AreEqual(originalUserCount + 1, newUserCount);
+        }
+    }
+}
