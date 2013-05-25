5132c355c3a2b890d2a0fd907a0c890447f7338c
unknown <andrey.agibalov@gmail.com>
2013-05-25 18:24:42 +0400

    Initial version

---
+++ LinFuExperiment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using LinFu.Proxy;
+
+namespace LinFuExperiment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new LinFu.DynamicObject();
+
+            IMagic m = new Magic().CreateDuck<IMagic>();
+            m.DoSomething();
+        }
+    }
+
+    public interface IMagic
+    {
+        void DoSomething();
+    }
+
+    public class Magic : DynamicObject
+    {
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            Console.WriteLine("something happened - {0}", binder.Name);
+            result = null;
+            return true;
+        }
+    }
+}
