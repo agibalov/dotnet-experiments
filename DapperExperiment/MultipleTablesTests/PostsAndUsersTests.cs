d92637d20782593a7d4b46e227f1575a05bc2af8
unknown <loki2302@loki2302.me>
2013-07-04 18:42:58 +0400

    One more test

--- DapperExperiment/MultipleTablesTests/PostsAndUsersTests.cs
+++ DapperExperiment/MultipleTablesTests/PostsAndUsersTests.cs
@@ -25,5 +25,31 @@ namespace DapperExperiment.MultipleTablesTests
         {
             BlogService.CreatePost(123, "sample post");
         }
+
+        [Test]
+        public void DifferentUsersHaveDifferentPosts()
+        {
+            var user1 = BlogService.CreateUser("user1");
+            var user1post1 = BlogService.CreatePost(user1.UserId, "user 1 post 1");
+            var user1post2 = BlogService.CreatePost(user1.UserId, "user 1 post 2");
+
+            var user2 = BlogService.CreateUser("user2");
+            var user2post1 = BlogService.CreatePost(user2.UserId, "user 2 post 1");
+            var user2post2 = BlogService.CreatePost(user2.UserId, "user 2 post 2");
+            var user2post3 = BlogService.CreatePost(user2.UserId, "user 2 post 3");
+
+            var userCount = BlogService.GetUserCount();
+            Assert.AreEqual(2, userCount);
+
+            var postCount = BlogService.GetPostCount();
+            Assert.AreEqual(5, postCount);
+
+            var posts = BlogService.GetAllPosts();
+            AssertEqualPosts(user1post1, posts[0]);
+            AssertEqualPosts(user1post2, posts[1]);
+            AssertEqualPosts(user2post1, posts[2]);
+            AssertEqualPosts(user2post2, posts[3]);
+            AssertEqualPosts(user2post3, posts[4]);
+        }
     }
 }
