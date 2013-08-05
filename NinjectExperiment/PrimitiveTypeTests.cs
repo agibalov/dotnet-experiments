70f80259f12367b6714e08584bb16c3cffc942f8
unknown <loki2302@loki2302.me>
2013-08-05 21:40:10 +0400

    Added primitive type tests

---
+++ NinjectExperiment/PrimitiveTypeTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using Ninject;
+
+namespace NinjectExperiment
+{
+    public class PrimitiveTypeTests
+    {
+        [Test]
+        public void CanInjectPrimitiveValuesUsingExplicitConstructorArgumentBindings()
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind<Service>().ToSelf().InSingletonScope().WithConstructorArgument("period", 123);
+            var service = kernel.Get<Service>();
+            Assert.AreEqual(123, service.Period);
+        }
+
+        [Test]
+        public void CanInjectPrimitveValuesUsingNamedConstructorArguments()
+        {
+            var kernel = new StandardKernel();
+
+            kernel.Bind<int>().ToConstant(123).Named("period");
+
+            var service = kernel.Get<Service2>();
+            Assert.AreEqual(123, service.Period);
+        }
+
+        class Service
+        {
+            public int Period { get; private set; }
+
+            public Service(int period)
+            {
+                Period = period;
+            }
+        }
+
+        class Service2
+        {
+            public int Period { get; private set; }
+
+            public Service2([Named("period")] int period)
+            {
+                Period = period;
+            }
+        }
+    }
+}
