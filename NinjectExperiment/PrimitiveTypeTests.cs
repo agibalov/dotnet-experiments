6e5f34ccc777119366250ae46352f0d220854785
unknown <loki2302@loki2302.me>
2013-08-05 21:50:08 +0400

    Added comment

--- NinjectExperiment/PrimitiveTypeTests.cs
+++ NinjectExperiment/PrimitiveTypeTests.cs
@@ -3,6 +3,41 @@ using Ninject;
 
 namespace NinjectExperiment
 {
+    public class ConfigurationTests
+    {
+        [Test]
+        public void Test()
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind<Configuration>().ToConstant(new Configuration
+                {
+                    Period = 123
+                });
+
+            kernel.Bind<int>().ToMethod(x => x.Kernel.Get<Configuration>().Period).Named("period");
+
+            var service = kernel.Get<Service>();
+            Assert.AreEqual(123, service.Period);
+        }
+
+        class Configuration
+        {
+            [Named("period")]
+            public int Period { get; set; }
+        }
+
+        class Service
+        {
+            public int Period { get; private set; }
+
+            // option: public Service([Named("period")] int thePeriod)
+            public Service(/*[Named("period")]*/ int period)
+            {
+                Period = period;
+            }
+        }
+    }
+
     public class PrimitiveTypeTests
     {
         [Test]
@@ -18,9 +53,7 @@ namespace NinjectExperiment
         public void CanInjectPrimitveValuesUsingNamedConstructorArguments()
         {
             var kernel = new StandardKernel();
-
             kernel.Bind<int>().ToConstant(123).Named("period");
-
             var service = kernel.Get<Service2>();
             Assert.AreEqual(123, service.Period);
         }
