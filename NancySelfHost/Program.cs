5e96988a15faa934f15d8b4f8d89beb4a630b08c
unknown <loki2302@loki2302.me>
2013-08-10 17:49:40 +0400

    Initial version

---
+++ NancySelfHost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace NancyExperiment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nancyHost = new NancyHost(new Uri("http://localhost:2302"));
+            nancyHost.Start();
+
+            Console.WriteLine("Listening at http://localhost:2302");
+            Console.WriteLine("Press any key to quit");
+
+            Console.ReadKey();
+            nancyHost.Stop();
+        }
+    }
+}
