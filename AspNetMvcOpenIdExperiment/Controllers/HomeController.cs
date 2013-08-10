5f8c6be28b150dfe60251c4f33d7ad799e2583a7
unknown <loki2302@loki2302.me>
2013-08-11 01:14:56 +0400

    Added Facebook authentication

--- AspNetMvcOpenIdExperiment/Controllers/HomeController.cs
+++ AspNetMvcOpenIdExperiment/Controllers/HomeController.cs
@@ -1,18 +1,76 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Net.Http;
+using System.Web.Mvc;
 using DotNetOpenAuth.Messaging;
+using DotNetOpenAuth.OAuth2;
 using DotNetOpenAuth.OpenId.Extensions.AttributeExchange;
 using DotNetOpenAuth.OpenId.RelyingParty;
+using Newtonsoft.Json;
 
 namespace AspNetMvcOpenIdExperiment.Controllers
 {
     public class HomeController : Controller
     {
+        private const string FacebookApplicationId = "219687484855349";
+        private const string FacebookApplicationSecret = "d58490eecd561a33275ccfeeabc0e5d5";
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult SignIn()
+        public ActionResult SignInWithFacebook()
+        {
+            var webServerClient = new WebServerClient(new AuthorizationServerDescription
+                {
+                    TokenEndpoint = new Uri("https://graph.facebook.com/oauth/access_token"),
+                    AuthorizationEndpoint = new Uri("https://graph.facebook.com/oauth/authorize"),
+                    ProtocolVersion = ProtocolVersion.V20
+                })
+                {
+                    ClientIdentifier = FacebookApplicationId,
+                    ClientCredentialApplicator = ClientCredentialApplicator.PostParameter(FacebookApplicationSecret)
+                };
+
+            var authorization = webServerClient.ProcessUserAuthorization(Request);
+            if (authorization == null)
+            {
+                var request = webServerClient.PrepareRequestUserAuthorization(new[] {"email"});
+                request.Send(HttpContext);
+                HttpContext.Response.End();
+                return null;
+            }
+
+            var authorizingHandler = webServerClient.CreateAuthorizingHandler(authorization);
+            var httpClient = new HttpClient(authorizingHandler);
+            var jsonResponse = httpClient
+                .GetAsync("https://graph.facebook.com/me?fields=email")
+                .Result
+                .Content
+                .ReadAsStringAsync()
+                .Result;
+            var facebookMe = JsonConvert.DeserializeObject<FacebookMe>(jsonResponse);
+            
+            ViewBag.Message = string.Format(
+                "Access token: {0}, JSON response: {1}, Email: {2}, Id: {3}", 
+                authorization.AccessToken, 
+                jsonResponse, 
+                facebookMe.Email,
+                facebookMe.Id);
+
+            return View();
+        }
+
+        public class FacebookMe
+        {
+            [JsonProperty("id")]
+            public string Id { get; set; }
+
+            [JsonProperty("email")]
+            public string Email { get; set; }
+        }
+
+        public ActionResult SignInWithGoogle()
         {
             using (var openIdRelyingParty = new OpenIdRelyingParty())
             {
