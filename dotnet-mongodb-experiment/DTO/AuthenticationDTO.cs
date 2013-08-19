61e1e08e91c21ee1f96dba68126b306c0d887cbc
unknown <loki2302@loki2302.me>
2013-08-19 10:41:55 +0400

    Initial version

---
+++ dotnet-mongodb-experiment/DTO/AuthenticationDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace dotnet_mongodb_experiment.DTO
+{
+    public class AuthenticationDTO
+    {
+        public String SessionToken { get; set; }
+        public String UserName { get; set; }
+    }
+}
---
+++ dotnet-mongodb-experiment/DTO/CompleteNoteDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace dotnet_mongodb_experiment.DTO
+{
+    public class CompleteNoteDTO
+    {
+        public string NoteId { get; set; }
+        public string Text { get; set; }
+        public BriefUserDTO User { get; set; }
+        public IList<BriefTagDTO> Tags { get; set; }
+    }
+}
