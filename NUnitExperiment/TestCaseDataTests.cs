51e2b53d50fcfe432642890c1ab00f49735e0785
unknown <loki2302@loki2302.me>
2013-08-21 10:36:19 +0400

    Added Test Source tests

---
+++ NUnitExperiment/TestCaseDataTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace NUnitExperiment
+{
+    public class TestCaseDataTests
+    {
+        [Test]
+        [TestCaseSource("SumTestCases")]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static IEnumerable SumTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(1, 2).Returns(3);
+                yield return new TestCaseData(2, 3).Returns(5);
+                yield return new TestCaseData(3, 10).Returns(13);
+            }
+        }
+    }
+}
---
+++ NUnitExperiment/TestCaseSourceDemo.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace NUnitExperiment
+{
+    public class TestCaseSourceDemo
+    {
+        [Test]
+        [TestCaseSource("SumCases")]
+        public void CanComputeSum(int a, int b, int result)
+        {
+            Assert.That(a + b, Is.EqualTo(result));
+        }
+
+        static readonly object[] SumCases =
+            {
+                new object[] {1, 2, 3},
+                new object[] {7, 3, 10}
+            };
+    }
+}
