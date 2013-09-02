d533f894314532e0a890457af9bf2593a5b86778
unknown <loki2302@loki2302.me>
2013-09-02 23:32:01 +0400

    Initial version

---
+++ PureAspNetWebApiWebAppExperiment/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web;
+using System.Web.Http;
+
+namespace PureAspNetWebApiWebAppExperiment
+{
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configuration.Routes.MapHttpRoute(
+                "BlogApi", 
+                "{action}", 
+                new { controller = "Post" });
+        }
+    }
+}
---
+++ PureAspNetWebApiWebAppExperiment/PostController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+
+namespace PureAspNetWebApiWebAppExperiment
+{
+    public class PostController : ApiController
+    {
+        [HttpGet]
+        public Post GetPost(int postId)
+        {
+            return new Post
+                {
+                    Title = string.Format("hello there #{0}", postId),
+                    Text = "the rest of the post goes here",
+                    Self = Url.Link("BlogApi", new { action = "GetPost", postId })
+                };
+        }
+    }
+
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Self { get; set; }
+    }
+}
