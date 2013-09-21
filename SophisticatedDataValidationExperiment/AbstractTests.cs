b4db83797cc0f99ce55bde850e78f52aaf8542b5
unknown <loki2302@loki2302.me>
2013-09-21 17:37:17 +0400

    Refactoring

---
+++ SophisticatedDataValidationExperiment/AbstractTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using FluentValidation;
+using NUnit.Framework;
+using Newtonsoft.Json;
+using Ninject;
+using Ninject.Extensions.Conventions;
+using SophisticatedDataValidationExperiment.Service.DTO;
+using SophisticatedDataValidationExperiment.Validation;
+
+namespace SophisticatedDataValidationExperiment
+{
+    public abstract class AbstractTests
+    {
+        private readonly OrderDTOValidator _orderDtoValidator;
+
+        protected AbstractTests()
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind(x => x
+                .FromThisAssembly()
+                .SelectAllClasses()
+                .InheritedFrom(typeof (AbstractValidator<>))
+                .BindToSelf()
+                .Configure(c => c.InSingletonScope()));
+
+            _orderDtoValidator = kernel.Get<OrderDTOValidator>();
+        }
+
+        protected void AssertAdequateOrder(OrderDTO order)
+        {
+            var validationResult = _orderDtoValidator.Validate(order);
+            if (validationResult.IsValid)
+            {
+                return;
+            }
+
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.Append(JsonConvert.SerializeObject(order, Formatting.Indented));
+            stringBuilder.AppendLine();
+            stringBuilder.AppendLine();
+            stringBuilder.AppendLine("is bad because:");
+            stringBuilder.AppendLine();
+
+            foreach (var error in validationResult.Errors)
+            {
+                stringBuilder.AppendFormat(
+                    "{0} {1} (actual: {2})\n",
+                    error.PropertyName,
+                    error.ErrorMessage,
+                    error.AttemptedValue);
+            }
+
+            Assert.Fail(stringBuilder.ToString());
+        }
+    }
+}
--- SophisticatedDataValidationExperiment/MegaServiceTests.cs
+++ SophisticatedDataValidationExperiment/MegaServiceTests.cs
@@ -1,66 +1,16 @@
-﻿using System.Text;
-using FluentValidation;
-using NUnit.Framework;
-using Newtonsoft.Json;
-using Ninject;
+﻿using NUnit.Framework;
 using SophisticatedDataValidationExperiment.Service;
-using SophisticatedDataValidationExperiment.Service.DTO;
-using SophisticatedDataValidationExperiment.Validation;
-using Ninject.Extensions.Conventions;
 
 namespace SophisticatedDataValidationExperiment
 {
-    public class MegaServiceTests
+    public class MegaServiceTests : AbstractTests
     {
-        private readonly OrderDTOValidator _orderDtoValidator;
-
-        public MegaServiceTests()
-        {
-            var kernel = new StandardKernel();
-            kernel.Bind(x => x
-                .FromThisAssembly()
-                .SelectAllClasses()
-                .InheritedFrom(typeof (AbstractValidator<>))
-                .BindToSelf()
-                .Configure(c => c.InSingletonScope()));
-
-            _orderDtoValidator = kernel.Get<OrderDTOValidator>();
-        }
-
         [Test]
         public void Test()
         {
             var service = new MegaService();
             var order = service.GetOrder();
             AssertAdequateOrder(order);
-        }
-
-        private void AssertAdequateOrder(OrderDTO order)
-        {
-            var validationResult = _orderDtoValidator.Validate(order);
-            if (validationResult.IsValid)
-            {
-                return;
-            }
-
-            var stringBuilder = new StringBuilder();
-
-            stringBuilder.Append(JsonConvert.SerializeObject(order, Formatting.Indented));
-            stringBuilder.AppendLine();
-            stringBuilder.AppendLine();
-            stringBuilder.AppendLine("is bad because:");
-            stringBuilder.AppendLine();
-
-            foreach (var error in validationResult.Errors)
-            {
-                stringBuilder.AppendFormat(
-                    "{0} {1} (actual: {2})\n",
-                    error.PropertyName,
-                    error.ErrorMessage,
-                    error.AttemptedValue);
-            }
-
-            Assert.Fail(stringBuilder.ToString());
         }
     }
 }
