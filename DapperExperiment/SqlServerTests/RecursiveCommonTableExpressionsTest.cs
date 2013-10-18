818ac5895282ab74e992eeb7e8964f1bd6c3626a
unknown <loki2302@loki2302.me>
2013-10-18 11:14:15 +0400

    Added CTE test

---
+++ DapperExperiment/SqlServerTests/RecursiveCommonTableExpressionsTest.cs
@@ -0,0 +1,66 @@
+using System.Linq;
+using Dapper;
+using NUnit.Framework;
+
+namespace DapperExperiment.SqlServerTests
+{
+    public class RecursiveCommonTableExpressionsTest : AbstractSqlServerTest
+    {
+        [Test]
+        public void CanUserRecursiveCte()
+        {
+            using (var connection = SqlServerDatabaseHelper.MakeConnection())
+            {
+                connection.Execute(@"
+                    create table Categories(
+	                    Id int primary key not null,
+	                    Name nvarchar(256) not null,
+	                    ParentCategoryId int foreign key references Categories(Id))");
+
+                connection.Execute(@"
+                    insert into Categories(Id, Name, ParentCategoryId) values(@id, @name, @parentId)",
+                    new { id = 1, name = "Me", parentId = (int?)null });
+
+                connection.Execute(@"
+                    insert into Categories(Id, Name, ParentCategoryId) values(@id, @name, @parentId)",
+                    new { id = 2, name = "Programming", parentId = 1 });
+
+                connection.Execute(@"
+                    insert into Categories(Id, Name, ParentCategoryId) values(@id, @name, @parentId)",
+                    new { id = 3, name = "Java", parentId = 2 });
+
+                connection.Execute(@"
+                    insert into Categories(Id, Name, ParentCategoryId) values(@id, @name, @parentId)",
+                    new { id = 4, name = "C++", parentId = 2 });
+            }
+
+            using (var connection = SqlServerDatabaseHelper.MakeConnection())
+            {
+                var levels = connection.Query(@"
+                    with cte(Level, Id, Name, ParentCategoryId) as (
+	                    select 0, Id, Name, ParentCategoryId from Categories where Id = @id
+	                    union all 
+	                    select cte.Level + 1, C.Id, C.Name, C.ParentCategoryId from Categories as C
+	                    join cte on C.Id = cte.ParentCategoryId
+                    )
+                    select Level, Id, Name, ParentCategoryId from cte order by Level desc;",
+                    new { id = 4 }).ToList();
+
+                Assert.AreEqual(2, levels[0].Level);
+                Assert.AreEqual(1, levels[0].Id);
+                Assert.AreEqual("Me", levels[0].Name);
+                Assert.AreEqual(null, levels[0].ParentCategoryId);
+
+                Assert.AreEqual(1, levels[1].Level);
+                Assert.AreEqual(2, levels[1].Id);
+                Assert.AreEqual("Programming", levels[1].Name);
+                Assert.AreEqual(1, levels[1].ParentCategoryId);
+
+                Assert.AreEqual(0, levels[2].Level);
+                Assert.AreEqual(4, levels[2].Id);
+                Assert.AreEqual("C++", levels[2].Name);
+                Assert.AreEqual(2, levels[2].ParentCategoryId);
+            }
+        }
+    }
+}
