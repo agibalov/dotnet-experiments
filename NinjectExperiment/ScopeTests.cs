c6a0e28bb9f43ed01c61f5e80eeb64d4c73b95ee
loki2302 <loki2302@loki2302.me>
2013-11-18 23:03:27 +0400

    Added scope test

---
+++ NinjectExperiment/ScopeTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using Ninject;
+
+namespace NinjectExperiment
+{
+    public class ScopeTests
+    {
+        [Test]
+        public void Test()
+        {
+            var kernel = new StandardKernel();
+
+            kernel.Bind<ScopedService>().ToSelf().InScope(x => ProcessingScope.Current);
+            kernel.Bind<ScopeObject>().ToMethod(x => ProcessingScope.Current);
+
+            var scopeObjectA = new ScopeObject { Name = "A" };
+            var scopeObjectB = new ScopeObject { Name = "B" };
+
+            // scope A
+            ProcessingScope.Current = scopeObjectA;
+            var serviceA1 = kernel.Get<ScopedService>();
+            Assert.AreSame(scopeObjectA, serviceA1.Scope);
+            
+            var serviceA2 = kernel.Get<ScopedService>();
+            Assert.AreSame(scopeObjectA, serviceA1.Scope);
+            
+            Assert.AreSame(serviceA1, serviceA2);
+
+            // scope B
+            ProcessingScope.Current = scopeObjectB;
+            var serviceB1 = kernel.Get<ScopedService>();
+            Assert.AreSame(scopeObjectB, serviceB1.Scope);
+            Assert.AreNotSame(serviceA1, serviceB1);
+        }
+
+        public class ScopeObject
+        {
+            public string Name { get; set; }
+
+            public override string ToString()
+            {
+                return string.Format("Scope{{{0}}}", Name);
+            }
+        }
+
+        public class ProcessingScope
+        {
+            public static ScopeObject Current { get; set; }
+        }
+
+        public class ScopedService
+        {
+            [Inject]
+            public ScopeObject Scope { get; set; }
+        }
+    }
+}
