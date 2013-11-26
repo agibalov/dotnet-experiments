188110e734e8ad00fb814629276cdfbf85b3eb6b
loki2302 <loki2302@loki2302.me>
2013-11-26 12:02:32 +0400

    Added test for custom controller/action resolution

--- AspNetWebApi2Experiment/Program.cs
+++ AspNetWebApi2Experiment/Program.cs
@@ -2,21 +2,8 @@
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            /*var baseAddress = "http://localhost:8080/";
-
-            using (WebApp.Start<Startup>(baseAddress))
-            {
-                var restClient = new RestClient(baseAddress);
-
-                var request = new RestRequest("GetPost", Method.GET);
-                request.AddParameter("postId", 123);
-                var response = restClient.Get<PostDto>(request);
-                Console.WriteLine("{0} {1}", response.Data.Id, response.Data.Content);
-
-                Console.ReadKey();
-            }*/
         }
     }
 }
