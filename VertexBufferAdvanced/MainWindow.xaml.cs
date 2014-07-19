f2832ea1404278d9b22fcedf22088589733886c4
loki2302 <loki2302@loki2302.me>
2014-07-20 00:23:38 +0400

    VertexBufferAdvanced now has colors

--- VertexBufferAdvanced/MainWindow.xaml.cs
+++ VertexBufferAdvanced/MainWindow.xaml.cs
@@ -21,10 +21,26 @@ namespace VertexBufferAdvanced
 
             var vertices = new[]
             {
-                new Vertex { X = -1, Y = -1, Z = 0 },
-                new Vertex { X = -1, Y = 1, Z = 0 },
-                new Vertex { X = 1, Y = 1, Z = 0 },
-                new Vertex { X = 1, Y = -1, Z = 0 }
+                new Vertex
+                {
+                    Position = new Vector3 { X = -1, Y = -1, Z = 0 }, 
+                    Color = new Color3 { R = 1, G = 0, B = 0 }
+                },
+                new Vertex
+                {
+                    Position = new Vector3 { X = -1, Y = 1, Z = 0 }, 
+                    Color = new Color3 { R = 0, G = 1, B = 0 }
+                },
+                new Vertex
+                {
+                    Position = new Vector3 { X = 1, Y = 1, Z = 0 }, 
+                    Color = new Color3 { R = 0, G = 0, B = 1 }
+                },
+                new Vertex
+                {
+                    Position = new Vector3 { X = 1, Y = -1, Z = 0 }, 
+                    Color = new Color3 { R = 1, G = 1, B = 0 }
+                }
             };
 
             var buf = MakeRaw(vertices);
@@ -34,8 +50,10 @@ namespace VertexBufferAdvanced
 
             _bufferId = bufs[0];
             gl.BindBuffer(OpenGL.GL_ARRAY_BUFFER, _bufferId);
-            gl.BufferData(OpenGL.GL_ARRAY_BUFFER, vertices.Length * Marshal.SizeOf(typeof(Vertex)), buf, OpenGL.GL_STATIC_DRAW);
-            gl.VertexAttribPointer(0, 0, OpenGL.GL_FLOAT, false, 0, IntPtr.Zero);
+            gl.BufferData(OpenGL.GL_ARRAY_BUFFER, vertices.Length * Marshal.SizeOf<Vertex>(), buf, OpenGL.GL_STATIC_DRAW);
+            gl.VertexPointer(3, OpenGL.GL_FLOAT, Marshal.SizeOf<Vertex>(), IntPtr.Zero);
+            gl.ColorPointer(3, OpenGL.GL_FLOAT, Marshal.SizeOf<Vertex>(), IntPtr.Zero + Marshal.SizeOf<Vector3>());
+            gl.BindBuffer(OpenGL.GL_ARRAY_BUFFER, 0);
 
             Marshal.FreeHGlobal(buf);
         }
@@ -45,12 +63,13 @@ namespace VertexBufferAdvanced
             var gl = args.OpenGl;
 
             gl.LookAt(0, 0, -5, 0, 0, 0, 0, 1, 0);
-
+            
             gl.BindBuffer(OpenGL.GL_ARRAY_BUFFER, _bufferId);
-            gl.VertexPointer(3, OpenGL.GL_FLOAT, 0, IntPtr.Zero);
             gl.EnableClientState(OpenGL.GL_VERTEX_ARRAY);
+            gl.EnableClientState(OpenGL.GL_COLOR_ARRAY);
             gl.DrawArrays(OpenGL.GL_QUADS, 0, 4);
             gl.DisableClientState(OpenGL.GL_VERTEX_ARRAY);
+            gl.DisableClientState(OpenGL.GL_COLOR_ARRAY);
         }
 
         private static IntPtr MakeRaw(Vertex[] vertices)
@@ -69,9 +88,24 @@ namespace VertexBufferAdvanced
         [StructLayout(LayoutKind.Sequential)]
         public struct Vertex
         {
+            public Vector3 Position;
+            public Color3 Color;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Vector3
+        {
             public float X;
             public float Y;
             public float Z;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Color3
+        {
+            public float R;
+            public float G;
+            public float B;
         }
     }
 }
