63d7195dac26e92d317976c0f41cb51b04a7fa92
loki2302 <loki2302@loki2302.me>
2014-07-20 15:44:42 +0400

    Added HelloUserControl

---
+++ HelloUserControl/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace HelloUserControl
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new AppViewModel
+            {
+                UserName1 = "loki2302",
+                UserName2 = "Andrey"
+            };
+        }
+    }
+
+    public class AppViewModel
+    {
+        public string UserName1 { get; set; }
+        public string UserName2 { get; set; }
+    }
+}
