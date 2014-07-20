6f9e395568aa8cd6e5ec8dd5b1ee3fa727db7aaf
loki2302 <loki2302@loki2302.me>
2014-07-20 17:07:05 +0400

    Added HelloCommand

---
+++ HelloCommand/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace HelloCommand
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new AppViewModel
+            {
+                ShowMessageCommand = new ShowMessageCommand()
+            };
+        }
+    }
+
+    public class AppViewModel
+    {
+        public ShowMessageCommand ShowMessageCommand { get; set; }
+    }
+
+    public class ShowMessageCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            MessageBox.Show("I am command");
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
