cbe1de31650a4ff39d086ed6c5632c60e1cce5fe
loki2302 <loki2302@loki2302.me>
2014-07-21 23:40:41 +0400

    For HelloCaliburnMicro, the ShellView is now Window instead of UserControl

--- HelloCaliburnMicro/ShellView.xaml.cs
+++ HelloCaliburnMicro/ShellView.xaml.cs
@@ -10,7 +10,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace HelloCaliburnMicro
@@ -18,7 +17,7 @@ namespace HelloCaliburnMicro
     /// <summary>
     /// Interaction logic for ShellView.xaml
     /// </summary>
-    public partial class ShellView : UserControl
+    public partial class ShellView : Window
     {
         public ShellView()
         {
