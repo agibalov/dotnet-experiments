2e432a802c1cfca1f3bf0bbf73790d9b096d3b2a
loki2302 <loki2302@loki2302.me>
2014-10-17 22:48:35 +0400

    Initial version

---
+++ SisoDbExperiment/InterfaceTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SisoDbExperiment
+{
+    public class InterfaceTest : AbstractSisoDbTest
+    {
+        [Test]
+        public void InterfaceSupportSeemsToBeBroken()
+        {
+            using (var session = Db.BeginSession())
+            {
+                session.Insert<INote>(new TextNote
+                {
+                    Title = "Text Note",
+                    Text = "I am text note"
+                });
+
+                session.Insert<INote>(new ImageNote
+                {
+                    Title = "Image Note",
+                    ImageUrl = "http://image.url"
+                });
+            }
+
+            var notes = Db.UseOnceTo().Query<INote>().ToList();
+            Assert.AreEqual(2, notes.Count);
+            Assert.Null(notes[0]);
+            Assert.Null(notes[1]);
+        }
+
+        public interface INote
+        {
+            Guid Id { get; set; }
+            string Title { get; set; }
+        }
+
+        public class TextNote : INote
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public string Text { get; set; }
+        }
+
+        public class ImageNote : INote
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public string ImageUrl { get; set; }
+        }
+    }
+}
