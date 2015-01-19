a1e39adb9af2781e42468109bc9cff5e50dd10a4
loki2302 <loki2302@loki2302.me>
2015-01-19 10:32:35 -0800

    Added a dummy sqlite-net test

---
+++ TodoListApp.Tests/DummyTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System.Threading.Tasks;
+using SQLite;
+
+// http://blog.tpcware.com/2014/05/universal-app-with-sqlite-part-2/
+
+namespace TodoListApp.Tests
+{
+    [TestClass]
+    public class DummyTest
+    {
+        private const string TestDbFileName = "1.db";
+        private SQLiteConnection _connection;
+
+        [TestInitialize]
+        public async Task CleanUpDatabase()
+        {
+            await SqliteHelper.DropDatabaseIfExists(TestDbFileName);
+            var databaseFilePath = await SqliteHelper.CreateDatabaseFilePath(TestDbFileName);
+            _connection = new SQLiteConnection(databaseFilePath);
+        }
+
+        [TestCleanup]
+        public void ForgetConnection()
+        {
+            _connection = null;
+        }
+
+        [TestMethod]
+        public void DummyScenarioWorks()
+        {
+            _connection.CreateTable<Note>();
+            _connection.Insert(new Note
+            {
+                Text = "hello"
+            });
+
+            Assert.AreEqual(1, _connection.Table<Note>().Count());
+        }
+    }
+}
---
+++ TodoListApp.Tests/Note.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace TodoListApp.Tests
+{
+    [Table("Notes")]
+    public class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
