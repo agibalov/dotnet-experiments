a862f3c4144fabda8aab4803cb562c09359f2db8
loki2302 <loki2302@loki2302.me>
2015-01-23 10:22:28 +0400

    Fixed DialogControl's bindings + its demo window now displays messageboxes, when buttons are clicked

--- HelloUserControl/MainWindow.xaml.cs
+++ HelloUserControl/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
 
 namespace HelloUserControl
 {
@@ -20,5 +22,36 @@ namespace HelloUserControl
     {
         public string UserName1 { get; set; }
         public string UserName2 { get; set; }
+
+        public ICommand OkCommand { get; set; }
+        public ICommand CancelCommand { get; set; }
+
+        public AppViewModel()
+        {
+            OkCommand = new ShowMessageBoxCommand("OK clicked!");
+            CancelCommand = new ShowMessageBoxCommand("Cancel clicked");
+        }
+    }
+
+    public class ShowMessageBoxCommand : ICommand
+    {
+        private readonly string _message;
+
+        public ShowMessageBoxCommand(string message)
+        {
+            _message = message;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            MessageBox.Show(_message);
+        }
+
+        public event EventHandler CanExecuteChanged;
     }
 }
