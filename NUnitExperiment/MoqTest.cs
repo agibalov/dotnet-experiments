dbf5972ad3471f9e6d0b11d3f23e3044a93fa3ff
loki2302 <loki2302@loki2302.me>
2015-02-21 17:38:18 +0400

    Added MoqTest

---
+++ NUnitExperiment/MoqTest.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace NUnitExperiment
+{
+    public class MoqTest
+    {
+        [Test]
+        public void Dummy()
+        {
+            var mock = new Mock<ICalculator>();
+            mock.Setup(c => c.Add(2, 3)).Returns(5);
+
+            var calculator = mock.Object;
+            Assert.AreEqual(5, calculator.Add(2, 3));
+        }
+
+        public interface ICalculator
+        {
+            int Add(int a, int b);
+        }
+    }
+}
