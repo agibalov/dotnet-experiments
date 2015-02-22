bb1486390c08fdf1fb44f45a784aebe5ed6bf121
loki2302 <loki2302@loki2302.me>
2015-02-22 13:31:57 +0400

    Added strict vs. loose behavior tests to MoqTest

--- NUnitExperiment/MoqTest.cs
+++ NUnitExperiment/MoqTest.cs
@@ -6,13 +6,41 @@ namespace NUnitExperiment
     public class MoqTest
     {
         [Test]
-        public void Dummy()
+        public void CanHaveACalculatorMock()
         {
-            var mock = new Mock<ICalculator>();
-            mock.Setup(c => c.Add(2, 3)).Returns(5);
+            var calculatorMock = new Mock<ICalculator>();
+            calculatorMock.Setup(c => c.Add(2, 3)).Returns(5);
 
-            var calculator = mock.Object;
+            var calculator = calculatorMock.Object;
             Assert.AreEqual(5, calculator.Add(2, 3));
+
+            calculatorMock.Verify(c => c.Add(2, 3), Times.Once);
+        }
+
+        [Test]
+        public void WhenUsingLooseBehaviorCallingMethodsWithoutSetupDoesNotFail()
+        {
+            var calculatorMock = new Mock<ICalculator>(MockBehavior.Loose);
+            var calculator = calculatorMock.Object;
+            
+            var result = calculator.Add(2, 3);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void WhenUsingStrictBehaviorCallingMethodsWithoutSetupFails()
+        {
+            var calculatorMock = new Mock<ICalculator>(MockBehavior.Strict);
+            var calculator = calculatorMock.Object;
+
+            try
+            {
+                calculator.Add(2, 3);
+                Assert.Fail();
+            }
+            catch (MockException)
+            {
+            }
         }
 
         public interface ICalculator
