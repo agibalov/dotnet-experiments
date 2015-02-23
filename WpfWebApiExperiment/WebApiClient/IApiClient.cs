02bfc765165b15dcd10d2f42a2d58f2c67570658
loki2302 <loki2302@loki2302.me>
2015-02-23 13:54:18 +0400

    Cleaning up

--- WpfWebApiExperiment/WebApiClient/IApiClient.cs
+++ WpfWebApiExperiment/WebApiClient/IApiClient.cs
@@ -4,9 +4,6 @@ namespace WpfWebApiExperiment.WebApiClient
 {
     public interface IApiClient
     {
-        /*List<NoteDTO> GetNotes();
-        NoteDTO GetNote(string id);*/
-
         TResult Execute<TResult>(IRestRequest restRequest)
             where TResult : new();
     }
