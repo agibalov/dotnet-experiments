f89ab0d0701d02cae8a661911022644d8c3447c0
loki2302 <loki2302@loki2302.me>
2015-03-06 21:00:53 +0400

    Added HelloMarkupExtension

---
+++ HelloMarkupExtension/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace HelloMarkupExtension
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    [MarkupExtensionReturnType(typeof(string))]
+    public class SumExtension : MarkupExtension
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return string.Format("{0} and {1} is {2}", A, B, A + B);
+        }
+    }
+}
