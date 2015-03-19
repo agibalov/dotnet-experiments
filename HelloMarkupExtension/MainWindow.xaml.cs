4a76468e5e412d8830a81f26b674fbf24a8070b0
loki2302 <loki2302@loki2302.me>
2015-03-19 21:53:54 +0400

    Added MakeButtonExtension

--- HelloMarkupExtension/MainWindow.xaml.cs
+++ HelloMarkupExtension/MainWindow.xaml.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Markup;
+using HelloMarkupExtension.Annotations;
 
 namespace HelloMarkupExtension
 {
@@ -9,6 +15,21 @@ namespace HelloMarkupExtension
         public MainWindow()
         {
             InitializeComponent();
+            var vm = new ViewModel();
+            DataContext = vm;
+
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        vm.CurrentTime = DateTime.Now.ToString();
+                    });
+                    
+                    Thread.Sleep(500);
+                }
+            }){IsBackground = true}.Start();
         }
     }
 
@@ -21,6 +42,44 @@ namespace HelloMarkupExtension
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
             return string.Format("{0} and {1} is {2}", A, B, A + B);
+        }
+    }
+
+    [MarkupExtensionReturnType(typeof(Button))]
+    public class MakeButtonExtension : MarkupExtension
+    {
+        public BindingBase Binding { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var b = new Button();
+            b.SetBinding(ContentControl.ContentProperty, Binding);
+            return b;
+        }
+    }
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private string _currentTime;
+
+        public string CurrentTime
+        {
+            get { return _currentTime; }
+            set
+            {
+                if (value == _currentTime) return;
+                _currentTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
